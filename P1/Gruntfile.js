77b28c972ab15f89a3dd778559c4373905c1b2dd
John Merigliano <jmerigliano@gmail.com>
2015-09-08 08:29:12 -0400

    updated grunt automation for images

--- P1/Gruntfile.js
+++ P1/Gruntfile.js
@@ -6,20 +6,53 @@ module.exports = function(grunt) {
 
         // minify css (this plugin can also concatenate files)
         cssmin: {
-          target: {
-            files: [{
-              expand: true,
-              cwd: 'css/',
-              src: ['*.css', '!*.min.css'],
-              dest: 'css/',
-              ext: '.min.css'
-            }]
-          }
+            target: {
+                files: [{
+                    expand: true,
+                    cwd: 'css/',
+                    src: ['*.css', '!*.min.css'],
+                    dest: 'css/',
+                    ext: '.min.css'
+                }]
+            }
+        },
+
+        responsive_images: {
+            dev: {
+                options: {
+                    engine: 'im',
+                    sizes: [{
+                        /* Change these */
+                        width: 224,
+                        suffix: '_small_1x',
+                        quality: 30
+                    }]
+                },
+
+                /*
+                You don't need to change this part if you don't change
+                the directory structure.
+                */
+                files: [{
+                    expand: true,
+                    src: ['*.{jpg,png}'],
+                    cwd: 'img/',
+                    dest: 'img/'
+                }]
+            }
+        },
+
+        /* Clear out the images directory if it exists */
+        clean: {
+            dev: {
+                src: ['img/*'],
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-responsive-images');
+    grunt.loadNpmTasks('grunt-contrib-clean');
 
-    // grunt.registerTask('default', ['cssmin', 'imagemin', 'name-changer']);
-    grunt.registerTask('default', ['cssmin']);
+    grunt.registerTask('default', ['cssmin', 'responsive_images']);
 };
