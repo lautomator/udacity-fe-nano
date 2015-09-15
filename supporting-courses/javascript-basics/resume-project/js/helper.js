7566fa922728724a96aa140f8f2ff69acd77bae6
John Merigliano <jmerigliano@gmail.com>
2015-09-15 14:42:36 -0400

    resume continues

--- supporting-courses/javascript-basics/resume-project/js/helper.js
+++ supporting-courses/javascript-basics/resume-project/js/helper.js
@@ -85,7 +85,7 @@ function logClicks(x,y) {
 }
 
 $(document).click(function(loc) {
-  // your code goes here!
+  logClicks(loc.pageX, loc.pageY);
 });
 
 
--- supporting-courses/javascript-basics/resume-project/js/resumeBuilder.js
+++ supporting-courses/javascript-basics/resume-project/js/resumeBuilder.js
@@ -19,27 +19,27 @@ var bio = {
 }
 
 var work = {
-    "position": [
+    "jobs": [
         {
-            "title": "job pos",
-            "employer": "company name",
-            "years": 3,
-            "city": "com city",
-            "state": "PA"
+            "title": "job pos 1",
+            "employer": "company Fred",
+            "years": "January 2015 -",
+            "location": "city, state",
+            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eros mauris, dictum ut mi eu, faucibus fringilla velit. Ut ac metus eleifend, vulputate arcu et, ultrices est. Sed sodales et lacus vel imperdiet. Suspendisse mollis felis quis cursus posuere. Etiam eget sapien metus. Nunc cursus porta accumsan. Ut pulvinar quis lectus vel condimentum. Mauris tempor eleifend tristique."
         },
         {
-            "title": "job pos",
-            "employer": "company name",
-            "years": 3,
-            "city": "com city",
-            "state": "PA"
+            "title": "job pos 2",
+            "employer": "company Wilma",
+            "years": "Date 2015 - Date 2015",
+            "location": "city, state",
+            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eros mauris, dictum ut mi eu, faucibus fringilla velit. Ut ac metus eleifend, vulputate arcu et, ultrices est. Sed sodales et lacus vel imperdiet. Suspendisse mollis felis quis cursus posuere. Etiam eget sapien metus. Nunc cursus porta accumsan. Ut pulvinar quis lectus vel condimentum. Mauris tempor eleifend tristique."
         },
         {
-            "title": "job pos",
-            "employer": "company name",
-            "years": 3,
-            "city": "com city",
-            "state": "PA"
+            "title": "job pos 3",
+            "employer": "company Bam Bam",
+            "years": "Date 2015 - Date 2015",
+            "location": "city, state",
+            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eros mauris, dictum ut mi eu, faucibus fringilla velit. Ut ac metus eleifend, vulputate arcu et, ultrices est. Sed sodales et lacus vel imperdiet. Suspendisse mollis felis quis cursus posuere. Etiam eget sapien metus. Nunc cursus porta accumsan. Ut pulvinar quis lectus vel condimentum. Mauris tempor eleifend tristique."
         }
     ]
 }
@@ -93,25 +93,46 @@ var formattedName = HTMLheaderName.replace('%data%', bio.name),
     formattedBioPic = HTMLbioPic.replace('%data%', bio.bioPic),
     formattedBioMsg = HTMLwelcomeMsg.replace('%data%', bio.welcomeMsg);
 
-
+// name header
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
+
+// contact info
 $("#topContacts").append(formattedMobile);
 $("#topContacts").append(formattedEmail);
 $("#topContacts").append(formattedGit);
 $("#topContacts").append(formattedTwitter);
 $("#topContacts").append(formattedLocation);
+
+// pic and message
 $("#topContacts").append(formattedBioPic);
 $("#topContacts").append(formattedBioMsg);
 
+// skills (optional)
 if (bio.skills && bio.skills != '') {
     // the skills header
     $("#header").append(HTMLskillsStart);
 
-    for (var i = 0; i < bio.skills.length; i++) {
+    for (item in bio.skills) {
         // the skills
-        $("#topContacts").append(HTMLskills.replace('%data%', bio.skills[i]));
+        $("#skills").append(HTMLskills.replace('%data%', bio.skills[item]));
     }
-
 }
 
+// work experience
+function displayWork() {
+    for (item in work.jobs) {
+        $("#workExperience").append(HTMLworkStart);
+        // employer and title
+        $(".work-entry:last").append(HTMLworkEmployer.replace('%data%', work.jobs[item].employer) +
+            HTMLworkTitle.replace('%data%', work.jobs[item].title));
+        // years
+        $(".work-entry:last").append(HTMLworkDates.replace('%data%', work.jobs[item].years));
+        // location
+        $(".work-entry:last").append(HTMLworkLocation.replace('%data%', work.jobs[item].location));
+        // description
+        $(".work-entry:last").append(HTMLworkDescription.replace('%data%', work.jobs[item].description));
+    }
+}
+
+displayWork();
