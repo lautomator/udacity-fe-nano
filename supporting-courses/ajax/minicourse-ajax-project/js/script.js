36aab739a353404fa7f505033b71af49e79dc306
John Merigliano <jmerigliano@gmail.com>
2015-12-09 15:52:42 -0500

    feat: google maps and nytimes api

--- supporting-courses/ajax/minicourse-ajax-project/js/script.js
+++ supporting-courses/ajax/minicourse-ajax-project/js/script.js
@@ -1,15 +1,15 @@
-
 function loadData() {
 
-    var $body = $('body');
-    var $wikiElem = $('#wikipedia-links');
-    var $nytHeaderElem = $('#nytimes-header');
-    var $nytElem = $('#nytimes-articles');
-    var $greeting = $('#greeting');
+    var $body = $('body'),
+        $wikiElem = $('#wikipedia-links'),
+        $nytHeaderElem = $('#nytimes-header'),
+        $nytElem = $('#nytimes-articles'),
+        $greeting = $('#greeting'),
+        $greetingMessage;
 
     // clear out old data before new request
-    $wikiElem.text("");
-    $nytElem.text("");
+    $wikiElem.text('');
+    $nytElem.text('');
 
     // clear error message, if any
     if ($('#messages').hasClass('has_warning')) {
@@ -20,7 +20,7 @@ function loadData() {
     // get the values of the form request
     var $street  = $('#street').val(),
         $city    = $('#city').val(),
-        $address = $city + ', ' + $street;
+        $address =  $street + ', ' + $city;
 
     // check that the fields are not blank
     if ($street === '' && $city === '') {
@@ -30,22 +30,39 @@ function loadData() {
         $('#messages').addClass('has_warning');
 
     } else {
-        // get the picture using google maps API
-        var $googlePic = 'https://maps.googleapis.com/maps/api/streetview?size=600x300&location=' + $street + ',' + $city,
+        // get the picture using Google Maps API
+        var $googlePic = 'https://maps.googleapis.com/maps/api/' +
+                'streetview?size=600x300&location=' + $address,
             $backgroundImage = '<img class="bgimg" src="' + $googlePic + '">';
-
-        // just a check ... (Preserve the log in the console to see the value)
-        console.log($backgroundImage);
 
         // append the image to the body
         $body.append($backgroundImage);
 
-        // update the greeting
-        $greeting.text('So, you want to live at: ' + $address);
+        if ($street === '') {
+            $greetingMessage = 'So, you want to live in: ' + $city;
+        } else if ($city === '') {
+            $greetingMessage = 'So, you want to live at: ' + $street;
+        } else {
+            $greetingMessage = 'So, you want to live at: ' + $address;
+        }
+        $greeting.text($greetingMessage);
 
-        // more APIs...
+        // NY Times API
+        var $nytApiKey = 'api-key=THIS_IS_A_SECRET',
+            $fq = 'source:("The New York Times") AND glocations.contains:("' + $city + '")',
+            $NYTimesURL = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?fq=' +
+                $fq + '&' + $nytApiKey;
+
+        $.getJSON($NYTimesURL, function(data) {
+            $.each(data.response.docs, function() {
+            $nytElem.append('<li><a href="' + this.web_url +
+                '" target="_blank">' + this.headline.main + '</a>' +
+                '<br>' +  this.snippet + '</li>');
+            });
+        }).error(function(e) {
+                $nytElem.append('<li>No data available.</li>');
+        });
     }
-
     return false;
 };
 
